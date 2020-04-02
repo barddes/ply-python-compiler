bc9b964be0b4bd64d2fbd798ec31e2425e29541e
Cesar Bueno <cesar@stoom.com.br>
2020-04-02 08:47:33 -0300

    Debug logs

--- parser.py
+++ parser.py
@@ -43,7 +43,7 @@ class Parser:
     def execute(self):
         if self._parser and self._input:
             self._lexer.reset()
-            self._parser.parse(self._input)
+            self._parser.parse(self._input, debug=self._debug)
 
     def parse(self, input):
         self._input = input
@@ -78,19 +78,19 @@ class Parser:
         """ declaration_list : declaration
                              | declaration_list declaration
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_declaration_list_opt(self, p):
         """ declaration_list_opt : declaration_list
                                  | empty
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_type_specifier_opt(self, p):
         """ type_specifier_opt : type_specifier
                                | empty
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_type_specifier(self, p):
         """ type_specifier : VOID
@@ -98,27 +98,27 @@ class Parser:
                            | INT
                            | FLOAT
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
 
     def p_declarator(self, p):
         """ declarator : pointer_opt direct_declarator
                        | direct_declarator
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_pointer_opt(self, p):
         """ pointer_opt : pointer
                         | empty
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_pointer(self, p):
         """ pointer : TIMES pointer
                     |  TIMES empty
 
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_direct_declarator(self, p):
         """ direct_declarator : identifier
@@ -127,35 +127,35 @@ class Parser:
                               | direct_declarator LPAREN parameter_list RPAREN
                               | direct_declarator LPAREN identifier_list_opt RPAREN
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_identifier(self, p):
         """ identifier : ID
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_constant_expression_opt(self, p):
         """ constant_expression_opt : constant_expression
                                     | empty
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_identifier_list(self, p):
         """ identifier_list : identifier
                             | identifier_list identifier
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_identifier_list_opt(self, p):
         """ identifier_list_opt : identifier_list
                                 | empty
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_constant_expression(self, p):
         """ constant_expression : binary_expression
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_binary_expression(self, p):
         """ binary_expression : cast_expression
@@ -173,13 +173,13 @@ class Parser:
                               | binary_expression AND binary_expression
                               | binary_expression OR binary_expression
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_cast_expression(self, p):
         """ cast_expression : unary_expression
                             | LPAREN type_specifier RPAREN cast_expression
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_unary_expression(self, p):
         """ unary_expression : postfix_expression
@@ -187,7 +187,7 @@ class Parser:
                              | MINUSMINUS unary_expression
                              | unary_operator cast_expression
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_postfix_expression(self, p):
         """ postfix_expression : primary_expression
@@ -197,13 +197,13 @@ class Parser:
                                | postfix_expression MINUSMINUS
         """
 
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_argument_expression_opt(self, p):
         """ argument_expression_opt : argument_expression
                                     | empty
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_primary_expression(self, p):
         """ primary_expression : identifier
@@ -211,32 +211,32 @@ class Parser:
                                | SCONST
                                | LPAREN expression RPAREN
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_constant(self, p):
         """ constant : INT_CONST
                      | CHAR_CONST
                      | FLOAT_CONST
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_expression(self, p):
         """ expression : assignment_expression
                        | expression COMMA assignment_expression
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_argument_expression(self, p):
         """ argument_expression : assignment_expression
                                 | argument_expression COMMA assignment_expression
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_assignment_expression(self, p):
         """ assignment_expression : binary_expression
                                   | unary_expression assignment_operator assignment_expression
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_assignment_operator(self, p):
         """ assignment_operator : EQUALS
@@ -246,7 +246,7 @@ class Parser:
                                 | PLUSEQUALS
                                 | MINUSEQUALS
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_unary_operator(self, p):
         """ unary_operator : ADDRESS
@@ -255,71 +255,72 @@ class Parser:
                            | MINUS
                            | EXMARK
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_parameter_list(self, p):
         """ parameter_list : parameter_declaration
                            | parameter_list COMMA parameter_declaration
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_parameter_declaration(self, p):
         """ parameter_declaration : type_specifier declarator
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_declaration(self, p):
         """ declaration : type_specifier init_declarator_list_opt SEMI
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_init_declarator_list_opt(self, p):
         """ init_declarator_list_opt : init_declarator_list
                                      | empty
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_init_declarator_list(self, p):
         """ init_declarator_list : init_declarator
                                  | init_declarator_list COMMA init_declarator
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_init_declarator(self, p):
         """ init_declarator : declarator
                             | declarator EQUALS initializer
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_initializer(self, p):
         """ initializer : assignment_expression
                         | LBRACE initializer_list RBRACE
                         | LBRACE initializer_list COMMA RBRACE
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_initializer_list(self, p):
         """ initializer_list : initializer
                              | initializer_list COMMA initializer
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_compound_statement(self, p):
         """ compound_statement : LBRACE declaration_list_opt statement_list_opt RBRACE
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
+
 
     def p_statement_list(self, p):
         """ statement_list : statement
                            | statement_list statement
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_statement_list_opt(self, p):
         """ statement_list_opt : statement_list
                                | empty
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_statement(self, p):
         """ statement : expression_statement
@@ -331,51 +332,51 @@ class Parser:
                       | print_statement
                       | read_statement
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_expression_statement(self, p):
         """ expression_statement : expression_opt SEMI
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_expression_opt(self, p):
         """ expression_opt : expression
                            | empty
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_selection_statement(self, p):
         """ selection_statement : IF LPAREN expression RPAREN statement
                                 | IF LPAREN expression RPAREN statement ELSE statement
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_iteration_statement(self, p):
         """ iteration_statement : WHILE LPAREN expression RPAREN statement
                                 | FOR LPAREN expression_opt SEMI expression_opt SEMI expression_opt RPAREN statement
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_jump_statement(self, p):
         """ jump_statement : BREAK SEMI
                            | RETURN expression_opt SEMI
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_assert_statement(self, p):
         """ assert_statement : ASSERT expression SEMI
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_print_statement(self, p):
         """ print_statement : PRINT LPAREN expression_opt RPAREN SEMI
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_read_statement(self, p):
         """ read_statement : READ LPAREN argument_expression RPAREN SEMI
         """
-        pass
+        p[0] = ('TEM QUE FAZER AINDA', 123, 321)
 
     def p_empty(self, p):
         """ empty :
--- teste.c
+++ teste.c
@@ -1,9 +1,10 @@
 int a = b + c;
 
-
-int main () {
-    int a = 0, b = 1;
-    char c = 3;
-
-    return 0;
-}
+//
+//int main int main=a+b;{
+//    int a = 0, b = 1;
+//    char c;
+//
+//    print(a);
+//    read(c);
+//}
